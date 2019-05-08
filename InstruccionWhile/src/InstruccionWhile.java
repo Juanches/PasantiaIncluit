8e1f69d5e4865627df9375c1eadf805581b3a944
JuanPasantia <juansalcedo26@gmail.com>
2019-05-08 10:51:46 -0300

    Github

---
+++ InstruccionWhile/src/InstruccionWhile.java
@@ -0,0 +1,31 @@
+
+public class InstruccionWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//int x=0;
+//while(x<=10)
+//{
+//	System.out.println ("el valor de x es " +x);
+//	x= x + 2;
+//	}	
+
+
+//boolean cond=true;
+//int x=0;
+//while(cond)
+//{
+//	System.out.println ("el valor de x es " +x);
+//	x= x + 2;
+//	if (x>8)
+//	cond = false;
+//}		
+		int x=0;
+		while(x<=15)
+		{
+			System.out.println ("el valor de x es " +x);
+			x= (x*2) + 3;
+			}	
+	}
+
+}
